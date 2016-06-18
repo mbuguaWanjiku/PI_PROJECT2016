1928ed39f46d410493c53ff66db3190df9c059b3
esw2015 <marcia_95_cost@hotmail.com>
2016-06-18 01:07:51 +0100

    h

---
+++ SistemaDomotico/SistemaDomotico/SistemaDomotico/Equipamento.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemaDomotico
+{
+    public partial class Equipamento : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //DropDownList myDropDownList = ListView1.InsertItem.FindControl("tipoEquipamento") as DropDownList;
+            //List<string> tipos = new List<string> { "GM", "TM", "AC" };
+            //myDropDownList.DataTextField = "Value";
+            //myDropDownList.DataValueField = "Id";
+            //myDropDownList.DataBind();
+
+            //myDropDownList.Items.Insert(0, new ListItem("Please select", ""));
+
+
+        }
+
+        protected void ListView1_ItemInserting(object sender, ListViewInsertEventArgs e)
+        {
+            //string sv = ((DropDownList)e.Item.FindControl("tipoEquipamento")).SelectedValue;
+           
+            //equipamentoDS.InsertParameters.Add("fk_compartimento", "1");
+
+        }
+    }
+}
---
+++ SistemaDomotico/SistemaDomotico/SistemaDomotico/consola.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemaDomotico
+{
+    public partial class sistemaMain : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ListView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
